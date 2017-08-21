38e2be7400977e91e20c542440a32ee289a905cf
heyulin <heyulin@smalltreemedia.com>
2017-08-21 10:01:36 +0800

    开创项目

---
+++ src/settings/dev.py
@@ -0,0 +1,13 @@
+from base import *
+
+
+DATABASES = {
+    'default': {
+        'NAME': 'liucheng',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',        
+      },
+    }
