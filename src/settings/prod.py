a34870ceb2b14d0b3bd4e06e9835bf602729fb33
heyulin <heyulin@smalltreemedia.com>
2017-08-24 15:22:28 +0800

    新增prod

---
+++ src/settings/prod.py
@@ -0,0 +1,13 @@
+from base import *
+
+
+DATABASES = {
+    'default': {
+        'NAME': 'liucheng',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'he123811',
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',        
+      },
+    }
