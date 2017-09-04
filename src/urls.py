98cb5c910046204a8730a9aa9518ee5b5b31fb6b
coblan <coblan163.com>
2017-09-05 00:14:25 +0800

    更新static文件

--- src/urls.py
+++ src/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     url(r'^_ajax/(?P<app>\w+)?/?$',director_views.ajax_views,name='ajax_url'),
     url(r'^_ajax/?$',director_views.ajax_views), 
     url(r'^_face/', include(face_urls)),
+    url(r'^_download/(?P<app>\w+)?/?$',director_views.donwload_views,name='download_url'),
     
     url(r'^orgnize/',include(organize_urls)),
     
